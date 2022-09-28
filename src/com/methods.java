f6ff991fe120848cf1c39145828ebed3792cd1f1
Mohmoody <syed.mahmoody2014@gmail.com>
2022-09-28 00:45:42 -0400

    first eclips commit

---
+++ src/com/methods.java
@@ -0,0 +1,27 @@
+package com;
+
+public class methods {
+    public static void main(String[] args) {
+
+        System.out.println(hasTeen(0, 0, 20));
+
+    }
+
+    public static boolean hasTeen(int num1, int num2, int num3) {
+        if (num1 >= 13 && num1 <= 19 || num2 >= 13 && num2 <= 19 || num3 >= 13 && num3 <= 19) {
+
+            return true;
+        }
+        return false;
+    }
+    public static boolean isTeen(int number) {
+        if (number >= 13 && number <= 19) {
+            return true;
+        }
+        return false;
+    }
+}
+
+
+
+
